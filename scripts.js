e4a2fd402a0428af72da076d12becb6505a4994f
JoseSNeto-yasaph <62401417+JoseSNeto-yasaph@users.noreply.github.com>
2023-09-29 04:19:20 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,71 @@
+alert("Hellou World!")
+
+let sum = (number1, number2) => {
+     return number1 + number2
+}
+
+
+
+let number = 2
+
+if(!isNaN(number)) {
+    alert("E um numero")
+} else {
+    alert("Não e um número")
+}
+
+
+
+let string = "abc";
+
+if (typeof string === 'string') {
+  alert('A variável é uma string.');
+} else {
+  alert('A variável não é uma string.');
+}
+
+
+
+let bool = "true";
+
+if (bool === 'true' || bool === 'false') {
+  alert('A string é uma representação de um booleano.');
+} else {
+  alert('A string não é uma representação de um booleano.');
+}
+
+
+
+let sub = (number1, number2) => {
+    return number1 - number2
+}
+
+
+
+let mult = (number1, number2) => {
+    return number1 * number2
+}
+
+
+
+let divi = (number1, number2) => {
+    return number1 / number2
+}
+
+
+
+let numberOne = 10
+
+if(numberOne % 2 ===0) {
+    alert("Número par!")
+} else {
+    alert("Não e um número par!")
+}
+
+let numberTwo = 9;
+
+if (numberOne % 2 === 1) {
+  console.log("É um número ímpar");
+} else {
+  console.log("Não é um número ímpar");
+}
